97abd9308d5cf42000e2d3341682324f95eac9e5
khtkok-cell <khtkok@gmail.com>
2026-03-25 01:02:59 +0100

    Fix indentation for ValueError in divide function

--- src/calculator.py
+++ src/calculator.py
@@ -6,6 +6,6 @@ def subtract(a, b):
 def divide(a, b):
 """Retourne la division de a par b. Lance une exception"""
 """ si elle detecte une erreur."""
-if b == 0:
-raise ValueError("Division par zero est impossible")
+ if b == 0:
+  raise ValueError("Division par zero est impossible")
 return a / b
